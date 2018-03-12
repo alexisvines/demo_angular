8dc21aa3c7796e93ea7b2e3d8c66ba9bcbf2de30
Alexis Freire <afreire@acl.cl>
2018-03-12 11:55:39 -0400

    Directiva ngSwitch

--- acl-demo-angular4/src/app/app.component.ts
+++ acl-demo-angular4/src/app/app.component.ts
@@ -9,13 +9,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Acl-Demo-Aplicacion';
   lugares:any = [
-  	{active:true, nombre: 'Santiago'},
-  	{active:false, nombre: 'San antonio'},
-  	{active:true, nombre: 'Temuco'},
-  	{active:true, nombre: 'Validivia'},
-  	{active:true, nombre: 'Talcahuano'},
-  	{active:true, nombre: 'Arica'},
-  	{active:true, nombre: 'Iquique'},
+  	{cercania:1,distancia: 1, active:true,  nombre: 'Santiago'},
+  	{cercania:1,distancia: 1.8, active:false, nombre: 'San antonio'},
+  	{cercania:1,distancia: 5, active:false, nombre: 'Temuco'},
+  	{cercania:2,distancia: 10, active:false, nombre: 'Validivia'},
+  	{cercania:2,distancia: 40, active:false, nombre: 'Talcahuano'},
+  	{cercania:3,distancia: 54, active:false, nombre: 'Arica'},
+  	{cercania:3,distancia: 120, active:false, nombre: 'Iquique'},
 
   ];
 
