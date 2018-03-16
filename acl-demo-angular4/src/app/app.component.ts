cdb9096df1e8c2430b8421a9fcc1b0245c894be0
Alexis Freire <afreire@acl.cl>
2018-03-16 13:55:31 -0400

    Se agrega libreria rxjs que implementa observables

--- acl-demo-angular4/src/app/app.component.ts
+++ acl-demo-angular4/src/app/app.component.ts
@@ -1,30 +1,37 @@
 import { Component } from '@angular/core';
 import {AutorizacionService} from "./services/autorizacion.services";
+import { Observable} from 'rxjs';
+import 'rxjs/Rx';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  title = 'Acl-Demo-Aplicacion';
-  loggedIn = false;
+	title = 'Acl-Demo-Aplicacion';
+	loggedIn = false;
+	email = '';
 
-  constructor(private autorizacionService:AutorizacionService){
-  	this.autorizacionService.isLogged()
-  	.subscribe((result)=>{
-  		if(result && result.uid){
-  			this.loggedIn = true;
-  		}else{
-  			this.loggedIn = false;
-  		}
+	constructor(private autorizacionService:AutorizacionService){
+		this.autorizacionService.isLogged()
+		.subscribe((result)=>{
+			if(result && result.uid){
+				this.loggedIn = true;
+				this.email = autorizacionService.getEmail();
+			}else{
+				this.loggedIn = false;
+			}
+		},(error)=>{
+			this.loggedIn = false;
+		})
+	}
 
-  	}, (error)=>{
-  		this.loggedIn = false;
-  	}
-
-  	})
-  }
+	logout(){
+		this.autorizacionService.logout()
+	}
 }
+
 
--- acl-demo-angular4/src/app/services/autorizacion.services.ts
+++ acl-demo-angular4/src/app/services/autorizacion.services.ts
@@ -1,10 +1,11 @@
- import { Injectable} from "@angular/core";
+import { Injectable} from "@angular/core";
 import{ AngularFireAuth } from "angularfire2/auth/auth";
+import { Router } from "@angular/router";
 
  @Injectable()
  export class AutorizacionService{
 
- 	constructor(private angularFireAuth:AngularFireAuth){
+ 	constructor(private angularFireAuth:AngularFireAuth, private router: Router){
  		this.isLogged();
  	}
  	
@@ -15,6 +16,7 @@ import{ AngularFireAuth } from "angularfire2/auth/auth";
  		.then((response)=>{
  			alert("Usuario registrado con exito");
  			console.log(response);
+ 		 this.router.navigate(['lugares'])
  		})
  		.catch((error)=>{
  			alert("Un error ha ocurrido");
@@ -24,19 +26,28 @@ import{ AngularFireAuth } from "angularfire2/auth/auth";
 
  	public registro = (email, password) =>{
  		console.log('metodo de registro');
- 		this.angularFireAuth.auth.createUserWithEmailAndPassword(email,pas)
+ 		this.angularFireAuth.auth.createUserWithEmailAndPassword(email,password)
  		.then((response)=>{
  			alert("Usuario registrado con exito");
  			console.log(response);
+ 			this.router.navigate(['lugares']);
  		})
  		.catch((error)=>{
  			alert("Un error ha ocurrido");
  			console.log(error);
  		})
  	}
-
-
  	public isLogged(){
  		return this.angularFireAuth.authState;
+ 	}
+
+ 	public logout(){
+ 		this.angularFireAuth.auth.signOut();
+ 		alert("session cerrada");
+ 		this.router.navigate(["/login"]);
+ 	}
+
+ 	public getEmail(){
+ 		return this.angularFireAuth.auth.currentUser.email;
  	}
  }
